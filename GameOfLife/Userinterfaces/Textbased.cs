ed5d36e7bcc4c15791b43b71643216bf78a58e27
svenons <gotvonisvens@gmail.com>
2024-03-09 20:01:01 +0100

    refactor some code

--- GameOfLife/Userinterfaces/Textbased.cs
+++ GameOfLife/Userinterfaces/Textbased.cs
@@ -19,58 +19,8 @@ namespace GameOfLife
             switch (choice)
             {
                 case "1":
-                    // Start new grid
-                    int rows = 0;
-                    do {
-                        Console.Clear();
-                        Console.WriteLine("Enter the number of rows: ");
-                        Console.Write(">> ");
-                        string userInput = Console.ReadLine()!;
-                        if (!string.IsNullOrWhiteSpace(userInput) && int.TryParse(userInput, out rows) && rows > 3 && rows <= 100)
-                        {
-                            break;
-                        }
-                    } while(true);
-
-                    int columns = 0;
-                    do {
-                        Console.Clear();
-                        Console.WriteLine("Enter the number of columns: ");
-                        Console.Write(">> ");
-                        string userInput = Console.ReadLine()!;
-                        if (!string.IsNullOrWhiteSpace(userInput) && int.TryParse(userInput, out columns) && columns > 3 && columns <= 100)
-                        {
-                            break;
-                        }
-                    } while(true);
-
-                    Grid grid = new Grid() { grid = new List<List<ICell>>() };
-                    grid.InitializeGrid(rows, columns);
-                    
-                    while(true)
-                    {
-                        List<List<bool>> status = grid.GetGridStatus();
-                        Console.WriteLine("Initial grid status:");
-                        for (int i = 0; i < rows; i++)
-                        {
-                            for (int j = 0; j < columns; j++)
-                            {
-                                if (status[i][j])
-                                {
-                                    Console.Write("O");
-                                }
-                                else
-                                {
-                                    Console.Write(".");
-                                }
-                            }
-                            Console.WriteLine();
-                        }
-                        Console.WriteLine("Press any key to advance the grid...");
-                        Console.ReadKey();
-                        grid.AdvanceGrid();
-                    }
-                    //break;
+                    StartNewGame();
+                    break;
                 case "2":
                     // Load existing grid from a file
                     break;
@@ -82,6 +32,86 @@ namespace GameOfLife
                     Console.WriteLine("Invalid choice. Please try again.\n");
                     MainMenu(false);
                     break;
+            }
+        }
+        public void StartNewGame()
+        {
+            // Start new grid
+            int rows = 0;
+            do {
+                Console.Clear();
+                Console.WriteLine("Enter the number of rows for the grid (4-100)");
+                Console.Write(">> ");
+                string userInput = Console.ReadLine()!;
+                if (!string.IsNullOrWhiteSpace(userInput) && int.TryParse(userInput, out rows) && rows > 3 && rows <= 100)
+                {
+                    break;
+                }
+            } while(true);
+
+            int columns = 0;
+            do {
+                Console.Clear();
+                Console.WriteLine("Enter the number of columns for the grid (4-100)");
+                Console.Write(">> ");
+                string userInput = Console.ReadLine()!;
+                if (!string.IsNullOrWhiteSpace(userInput) && int.TryParse(userInput, out columns) && columns > 3 && columns <= 100)
+                {
+                    break;
+                }
+            } while(true);
+
+            Grid grid = new Grid() { grid = new List<List<ICell>>() };
+            grid.InitializeGrid(rows, columns);
+            
+            while(true)
+            {
+                List<List<bool>> status = grid.GetGridStatus();
+                Console.WriteLine("Initial grid status:");
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        if (status[i][j])
+                        {
+                            Console.Write("O");
+                        }
+                        else
+                        {
+                            Console.Write(".");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Press 'N' to advance to the next generation.");
+                Console.WriteLine("Press 'S' to save the current grid state to a file.");
+                Console.WriteLine("Press 'X' to exit the simulation.");
+                var key = Console.ReadKey();
+
+                do {
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.N:
+                            grid.AdvanceGrid();
+                            break;
+                        case ConsoleKey.S:
+                            Console.WriteLine("Enter the file name to save the grid to:");
+                            Console.Write(">> ");
+                            string fileName = Console.ReadLine()!;
+                            // Save grid to file
+                            //JsonStorage storage = new JsonStorage();
+                            //storage.SaveGrid(grid, fileName);
+                            break;
+                        case ConsoleKey.X:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            key = Console.ReadKey();
+                            break;
+                    }
+                } while (key.Key != ConsoleKey.N && key.Key != ConsoleKey.S && key.Key != ConsoleKey.X);
             }
         }
     }
